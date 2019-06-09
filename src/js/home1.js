9f59b50625202bed1d5ceef21207079949111e0a
Rafael Lopez <rafael.iskoat@gmail.com>
2019-06-08 22:05:27 -0500

    peticiones hacia una API con promises y tambien lo hicimos con async await

---
+++ src/js/home1.js
@@ -0,0 +1,29 @@
+
+
+    // utilizando async y await
+        // const fetchAPI = async () => {
+
+        //        try {
+
+        //            const data = await fetch('https://randomuser.me/api/',{method: 'GET'})
+        //            const dataResponse = await data.json()
+        //            const response = dataResponse
+
+        //            console.log(response.results[0]);
+
+        //        } catch (error) {
+        //            console.log(`Ha ocurrido algun problema ${error}`)
+        //        }
+        // }
+
+    // utilizando promises
+         const fetchAPI = () => {
+
+                    fetch('https://randomuser.me/api/',{method: 'GET'})
+                   .then(dataResponse => dataResponse.json() )
+                   .then(response => console.log(response.results[0]) )
+                   .catch(err => console.log(`Ha ocurrido algun problema ${err}`) ) 
+
+        }
+
+        fetchAPI()
