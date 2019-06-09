b8246e72f8d0770ce999e27667293df33ec58038
Rafael Lopez <rafael.iskoat@gmail.com>
2019-06-08 20:57:51 -0500

    utilizamos promises

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,36 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
 
-let cambia = "@LeonidasEsteban"
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+
+                            // resolver rechazar
+   const getUserAll = new Promise((resolve, reject)=> {
+          let response = true
+          setTimeout(() => {
+            return response ? resolve(`El request se hizo de manera exitosa y retorno una respuesta, get user all (2)`) : reject(`Error, el request ha fallado`)
+          }, 5000)
+   })
+                            // resolver rechazar
+   const getUser = new Promise((resolve, reject)=> {
+          let response = true
+          setTimeout(() => {
+            return response ? resolve(`El request se hizo de manera exitosa y retorno una respuesta get user (1)`) : reject(`Error, el request ha fallado`)
+          }, 3000)
+   })
+  
+    // getUser
+    // //todo salio bien
+    // .then(response => console.log(response) )
+    
+    // //todo salio mal
+    // .catch(errMessage =>  console.log(errMessage) )
+
+  // el Promise.race es un carrera de promesas la cual ejecutara la promesa que termine primero  
+    Promise.race([
+      getUserAll,
+      getUser
+    ])
+    // tenemos que manejar el then y catch de la siguiente promesa
+    //todo salio bien
+    .then(response => console.log(response) )
+  
+    //todo salio mal
+    .catch(errMessage =>  console.log(errMessage) )
